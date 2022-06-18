66e7d05177f50eac4f75242be65ac6903f17891e
Renan Rodrigues <renan.rodrigues.3@globalhitss.com.br>
2022-06-18 12:53:35 -0300

    first commit

---
+++ src/main/java/br/com/agama/AgamaLibApplication.java
@@ -0,0 +1,13 @@
+package br.com.agama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgamaLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgamaLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agama/AgamaLibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgamaLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
